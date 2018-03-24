44f754e3f852f272fd7a4fa0a5a3ab08e48f7581
fiendishTepes <fiendish.tepes@gmail.com>
2018-03-24 08:45:13 +0700

    000

--- test/input.php
+++ test/input.php
@@ -1,8 +1,23 @@
-<?php include 'cal.php'; $inp = new cal(); $inp->cals($_POST['arr']); ?>
-<form method="post" name="fm" id="fm">
-    <input type="text" name="arr['name']">
-    <input type="text" name="arr['last']">
-    <input type="text" name="arr['email']">
-    <input type="text" name="arr['phone']">
-    <button type="submit">click</button>
-</form>
+<?php 
+    define('HOST', 'localhost');
+    define('USER', 'root');
+    define('PASS', '123456789');
+    define('DB', 'db_test1');
+    define('TBL', 'tbl_register2');
+    
+    
+    class test
+    {
+        public function testSql()
+        {
+            $con = new mysqli(HOST,USER,PASS,DB);
+            $rs = $con->query('SELECT * FROM '.TBL)->fetch_array();
+            return $rs;
+        }
+    }
+    $connect = new mysqli(HOST,USER,PASS,DB);
+    $rs = $connect -> query("SELECT * FROM tbl_register2");
+					$total = $rs -> num_rows;
+					$page = $connect -> query("SELECT * FROM tbl_register2 ORDER BY regId
+												ASC LIMIT $start_page,$limit_page");
+?>
